eec5c1593cadcbfb7aec741e29e04f930e8e89f0
Ash-39284 <ashroberts98@icloud.com>
2026-01-29 13:33:31 +0000

    Add home page search code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-
 // google maps API key
 // AIzaSyAJoOGB75_bpzjqUae_aj1kBl_JIZJdu4I
+
+
+// Home page search feature
+
+$('#searchForm').on('submit', function (e) {
+  e.preventDefault();
+
+  localStorage.setItem(
+    'searchCity',
+    $('#destination').val().toLowerCase().trim()
+  );
+
+  localStorage.setItem(
+    'searchGuests',
+    $('#guests').val()
+  );
+
+  window.location.href = 'destinations.html';
+});
+
